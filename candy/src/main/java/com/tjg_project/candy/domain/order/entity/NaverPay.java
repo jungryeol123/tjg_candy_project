3a206871490f69d6327afd52ac3f48a4e2052b02
unknown <dbsgywls667@gmail.com>
2025-11-02 01:45:27 +0900

    네이버페이 구현

---
+++ candy/src/main/java/com/tjg_project/candy/domain/order/entity/NaverPay.java
@@ -0,0 +1,36 @@
+package com.tjg_project.candy.domain.order.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class NaverPay {
+    private String orderId;
+    //    private String userId;
+    private Long id;
+    private String itemName;
+    private String qty;
+    private String totalAmount;
+    private KakaoPay.Receiver receiver;
+    private KakaoPay.PaymentInfo paymentInfo;
+    private List<Long> cidList;
+
+    @Data
+    public static class Receiver {
+        private String name;
+        private String phone;
+        private String zipcode;
+        private String address1;
+        private String address2;
+        private String memo;
+    }//Receiver
+
+    @Data
+    public static class PaymentInfo {
+        private int shippingFee;
+        private int discountAmount;
+        private int totalAmount;
+    }
+}
