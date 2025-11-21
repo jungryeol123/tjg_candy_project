486f3e4ba4cc88d41dd0dca8e6579313037ac9e3
LEE <lsslcs01@gmail.com>
2025-11-21 15:59:03 +0900

    20251121 상품 편집 이미지 경로 수정

---
+++ candy/src/main/java/com/tjg_project/candy/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.tjg_project.candy.global.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // URL:  http://localhost:8080/product/파일명
+        // 실제 파일 위치: C:/uploads/product/파일명
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + uploadDir + "/");
+    }
+}
