40c2f642481f2873ab36bb73b50e7c9d383a51a5
shyun <exriik@gmail.com>
2025-11-24 18:47:34 +0900

    파일 추가

---
+++ candy/src/main/java/com/tjg_project/candy/domain/excel/dto/ForecastExcelRow.java
@@ -0,0 +1,10 @@
+package com.tjg_project.candy.domain.excel.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ForecastExcelRow {
+    private String date; // yyyy-MM-dd
+    private Integer value; // 예측값
+}
